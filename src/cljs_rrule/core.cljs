7e02ce38c0be1403c13703c593d566ccd2754d85
eyelidlessness <gnosis@gmail.com>
2016-06-13 21:51:15 -0700

    Initial library implementation

--- src/cljs_rrule/core.cljs
+++ src/cljs_rrule/core.cljs
@@ -1,4 +1,186 @@
 (ns cljs-rrule.core
-  (:require [rrule.js]))
+  (:refer-clojure :exclude [frequencies])
+  (:require [clojure.set :refer [union]]
+            [rrule.js]))
 
-(def RRule js/RRule)
+(def frequencies
+  #{:secondly
+    :minutely
+    :hourly
+    :daily
+    :weekly
+    :monthly
+    :yearly})
+
+(def days
+  #{:su
+    :mo
+    :tu
+    :we
+    :th
+    :fr
+    :sa})
+
+(def constants
+  (union frequencies days))
+
+(defn keyword->constant-name [kw]
+  (let [s (name kw)]
+    (.toUpperCase s)))
+
+(def keyword->constant
+  (reduce
+    (fn [acc kw]
+      (let [s (keyword->constant-name kw)
+            constant (aget js/RRule s)]
+        (assoc acc kw constant)))
+    {}
+    constants))
+
+(def constant->keyword
+  (reduce
+    (fn [acc [k v]]
+      (let [field (if (frequencies k) :freq :byweekday)]
+        (assoc-in acc [field v] k)))
+    {}
+    keyword->constant))
+
+(defn replace-constants [m]
+  (reduce
+    (fn [acc [k v]]
+      (assoc acc k (keyword->constant v v)))
+    {}
+    m))
+
+(defn restore-constant [k v]
+  (let [maybe-keyword (get-in constant->keyword [k v])]
+    (or maybe-keyword v)))
+
+(defn restore-constants [m]
+  (reduce
+    (fn [acc [k v]]
+      (assoc acc k (restore-constant k v)))
+    {}
+    m))
+
+(defn rrule-options* [k js-rrule]
+  (-> js-rrule
+      (aget k)
+      (js->clj :keywordize-keys true)
+      restore-constants))
+
+(def rrule-options
+  (partial rrule-options* "options"))
+
+(def rrule-original-options
+  (partial rrule-options* "origOptions"))
+
+(declare rrule)
+
+(deftype RRule [js-rrule prev-start]
+  ILookup
+  (-lookup [this k]
+    (-lookup this k nil))
+  (-lookup [_ k not-found]
+    (let [s (name k)
+          options (aget js-rrule "options")
+          v (->> s
+                 (aget options)
+                 (restore-constant k)
+                 js->clj)]
+      (or v not-found)))
+
+  IAssociative
+  (-assoc [_ k v]
+    (let [options (rrule-original-options js-rrule)
+          with-k (assoc options k v)]
+      (rrule with-k)))
+
+  IMap
+  (-dissoc [_ k]
+    (let [options (rrule-original-options js-rrule)
+          without-k (dissoc options k)]
+      (rrule without-k)))
+
+  ISeq
+  (-first [_]
+    (let [include-start? (nil? prev-start)
+          start (or prev-start (aget js-rrule "options" "dtstart"))]
+      (.after js-rrule start include-start?)))
+
+  (-rest [this]
+    (RRule. js-rrule (first this)))
+
+  ISeqable
+  (-seq [this]
+    (lazy-cat
+      [(first this)]
+      (seq (rest this)))))
+
+(defn rrule [m]
+  (let [js-rrule (js/RRule. (-> m replace-constants clj->js))]
+    (->RRule js-rrule nil)))
+
+(declare rrule-set)
+
+(deftype RRuleSet [js-rrule-set init-start prev-start]
+  ISeq
+  (-first [_]
+    (let [include-start? (nil? prev-start)
+          start (or prev-start init-start)]
+      (.after js-rrule-set start include-start?)))
+
+  (-rest [this]
+    (RRuleSet. js-rrule-set nil (first this)))
+
+  ISeqable
+  (-seq [this]
+    (lazy-cat
+      [(first this)]
+      (seq (rest this))))
+
+  ICollection
+  (-conj [_ rrule]
+    (let [rrules (set (aget js-rrule-set "_rrule"))
+          with-v (conj rrules rrule)]
+      (rrule-set with-v)))
+
+  ISet
+  (-disjoin [this rrule]
+    (let [rrules (set (map rrule-original-options (aget js-rrule-set "_rrule")))
+          without-v (disj rrules rrule)]
+      (rrule-set without-v))))
+
+(defn ->js-rrule [x]
+  (cond
+    (instance? js/RRule x)
+    x
+
+    (instance? RRule x)
+    (.-js-rrule x)
+
+    (map? x)
+    (-> x rrule .-js-rrule)
+
+    :else
+    (throw (ex-info "Invalid RRule argument" {:data x}))))
+
+(defn earliest [& dates]
+  (->> dates
+       (sort-by #(.getTime %))
+       first))
+
+(defn rrule-set [rrules]
+  (let [js-rrule-set (js/RRuleSet.)
+        js-rrules (map ->js-rrule rrules)
+        init-start (reduce
+                     (fn [acc rrule]
+                       (let [dtstart (aget rrule "options" "dtstart")]
+                         (if acc
+                             (earliest acc dtstart)
+                             dtstart)))
+                     nil
+                     js-rrules)]
+    (doseq [js-rrule js-rrules]
+      (.rrule js-rrule-set js-rrule))
+    (->RRuleSet js-rrule-set init-start nil)))
--- test/cljs_rrule/core_test.cljs
+++ test/cljs_rrule/core_test.cljs
@@ -1,10 +1,88 @@
 (ns cljs-rrule.core-test
-  (:require-macros [speclj.core :refer [describe it should should-not run-specs]])
+  (:require-macros [speclj.core :refer [describe it should=]])
   (:require [speclj.core]
-            [cljs-rrule.core :refer [RRule]]))
+            [cljs-rrule.core :as rrule]))
 
-(describe "RRule is available"
-  (it "has access to js/RRule"
-    (should-not (nil? RRule))))
+(describe "RRule"
+  (it "supports looking up fields by keyword"
+    (let [rrule (rrule/rrule {:byhour [1 3]})]
+      (should= [1 3] (:byhour rrule))))
 
-(run-specs)
+  (it "accepts keywords for constants"
+    (let [rrule (rrule/rrule {:freq :weekly})]
+      (should= :weekly (:freq rrule))))
+
+  (it "provides the first date"
+    (let [d (js/Date. 2006 6 6)
+          rrule (rrule/rrule {:freq :weekly :dtstart d})]
+      (should= d (first rrule))))
+
+  (it "provides the next date"
+    (let [d (js/Date. 2006 6 6)
+          rrule (rrule/rrule {:freq :weekly :dtstart d})
+          rest-rrule (rest rrule)]
+      (should= (js/Date. 2006 6 13) (first rest-rrule))))
+
+  (it "gets a lazy sequence of dates"
+    (let [d (js/Date. 2006 6 6)
+          rrule (rrule/rrule {:freq :weekly :dtstart d})
+          ds [(js/Date. 2006 6 6)
+              (js/Date. 2006 6 13)
+              (js/Date. 2006 6 20)
+              (js/Date. 2006 6 27)]]
+      (should= ds (take 4 rrule))))
+
+  (it "supports IAssoc assoc"
+    (let [d (js/Date. 2006 6 6)
+          rrule (rrule/rrule {:freq :weekly :dtstart d})
+          with-hour (assoc rrule :byhour 12)]
+      (should= (js/Date. 2006 6 6 12) (first with-hour))))
+
+  (it "supports IMap dissoc"
+    (let [d (js/Date. 2006 6 6)
+          rrule (rrule/rrule {:freq :weekly :byhour 12 :dtstart d})
+          without-hour (dissoc rrule :byhour)]
+      (should= (js/Date. 2006 6 6) (first without-hour)))))
+
+(describe "RRuleSet"
+  (it "provides the first date"
+    (let [d (js/Date. 2006 6 6)
+          rrule0 {:freq :weekly :byweekday :th :dtstart d}
+          rrule1 {:freq :weekly :byweekday :sa :dtstart d}
+          rrules (rrule/rrule-set [rrule0 rrule1])]
+      (should= d (first rrules))))
+
+  (it "provides the next date"
+    (let [d (js/Date. 2006 6 6)
+          rrule0 {:freq :weekly :byweekday :th :dtstart d}
+          rrule1 {:freq :weekly :byweekday :sa :dtstart d}
+          rrules (rrule/rrule-set [rrule0 rrule1])
+          rest-rrules (rest rrules)]
+      (should= (js/Date. 2006 6 8) (first rest-rrules))))
+
+  (it "gets a lazy sequence of dates"
+    (let [d (js/Date. 2006 6 6)
+          rrule0 {:freq :weekly :byweekday :th :dtstart d}
+          rrule1 {:freq :weekly :byweekday :sa :dtstart d}
+          rrules (rrule/rrule-set [rrule0 rrule1])
+          ds [(js/Date. 2006 6 6)
+              (js/Date. 2006 6 8)
+              (js/Date. 2006 6 13)
+              (js/Date. 2006 6 15)]]
+      (should= ds (take 4 rrules))))
+
+  (it "supports ICollection conj"
+    (let [d (js/Date. 2006 6 6)
+          rrule0 {:freq :weekly :byweekday :th :dtstart d}
+          rrule1 {:freq :weekly :byweekday :sa :dtstart d}
+          rrules (rrule/rrule-set [rrule0])
+          with-rrule1 (conj rrules rrule1)]
+      (should= (js/Date. 2006 6 8) (second with-rrule1))))
+
+  (it "supports ISet disj"
+    (let [d (js/Date. 2006 6 6)
+          rrule0 {:freq :weekly :byweekday :th :dtstart d}
+          rrule1 {:freq :weekly :byweekday :sa :dtstart d}
+          rrules (rrule/rrule-set [rrule0 rrule1])
+          without-rrule0 (disj rrules rrule0)]
+      (should= (js/Date. 2006 6 8) (first without-rrule0)))))
